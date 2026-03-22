4a4c8d4136c788ac2c40acc26ab7f1edaef2a64d
Chang <cliu81530@gmail.com>
2026-03-23 00:12:29 +0100

    add validation for user rental limits

---
+++ Services/RentalService.cs
@@ -0,0 +1,30 @@
+using APBD_TASK2.Models;
+using APBD_TASK2.Enums;
+using APBD_TASK2.Interfaces;
+
+namespace APBD_TASK2.Services
+{
+    public class RentalService : IRentalService
+    {
+        private readonly List<Rental> _rentals = new();
+        private readonly List<Equipment> _equipmentInventory = new();
+        private readonly List<User> _users = new();
+
+        public void RentEquipment(User renter, Equipment item, DateTime rentalDate, int durationDays)
+        {
+            if (!item.IsAvailable())
+                throw new InvalidOperationException($"Equipment{item.Name} is not available.");
+            
+            int currentCount = _rentals.Count(r => r.Renter.Id == renter.Id && !r.IsReturned);
+            if (currentCount >= renter.MaxRentalLimit)
+                throw new InvalidOperationException($"User {renter.Name} has reached the maximum rental limit of {renter.MaxRentalLimit}.");
+            
+            item.Status = EquipmentStatus.Rented;
+            var rental = new Rental(renter, item, rentalDate, durationDays);
+            _rentals.Add(rental);
+            Console.WriteLine($"User {renter.Name} rented {item.Name} on {rentalDate:d} for {durationDays} days.");
+        }
+
+
+    }
+}
